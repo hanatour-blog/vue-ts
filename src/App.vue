f93d133a7252c20e1c68a11f4873d98da57034b2
hyungkiShin <tkaqkeldk@nate.com>
2022-08-08 23:52:07 +0900

    [fix] prettier 설정, 및 기타 설정 수정

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TodoItems />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoItems from '@/components/TodoItems.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
+    TodoItems,
   },
-};
+}
 </script>
 
 <style>
---
+++ src/components/TodoItems.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItems',
+  props: {},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
