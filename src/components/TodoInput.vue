72e00f9e580120418519a2877c48e12d7834f5bc
A02053 <A02053@hanatour.com>
2022-08-10 23:19:30 +0900

    test

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,21 +1,27 @@
 <template>
   <div>
     <label for="todo-input">오늘 할일: </label>
-    <input type="text"/>
-    <button>추가</button>
+    <input type="text" v-model="title"/>
+    <button @click.prevent="createData">추가</button>
   </div>
 </template>
 
 <script>
+import index from '@/api/index'
 export default {
   name: 'TodoInput',
-  props: {
-    value: {
-      type: String,
-      required: true,
-    },
-  },
+  data(){
+    return{
+      title : ''
+    }
+  }, 
   methods: {
+    createData(){
+      const inputData = {
+        title : this.title
+      }
+      index.createItem(inputData)
+    }
   },
 }
 </script>
